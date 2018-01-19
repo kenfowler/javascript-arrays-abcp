aca340144bda0d957a5cd85578138192879966a5
Kenneth Fowler <kenfowler@comcast.net>
2018-01-19 20:37:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,5 +3,5 @@ function addElementToBeginningOfArray(array, element) {
   return array(element,...array)
 }
 function destructivelyAddElementToBeginningOfArray(array,element) {
-  return array.unshift(element)
+  return [element,...array]
 }
