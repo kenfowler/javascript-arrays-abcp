1109c2a1f5367e38488a5f2de7570c2acc4f9153
Kenneth Fowler <kenfowler@comcast.net>
2018-01-19 21:29:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,8 +17,7 @@ function accessElementInArray(array,index) {
   return array[index]
 }
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array.shift()
-  return
+  return array.shift()
 }
 function removeElementFromBeginningOfArray(array) {
   return array.slice[1,array.length-1]
