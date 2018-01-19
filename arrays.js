319d7b1a391b3909ad2a1f48e1dac0db45d2922d
Kenneth Fowler <kenfowler@comcast.net>
2018-01-19 21:00:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,18 +3,24 @@ function addElementToBeginningOfArray(array, element) {
   return [element,...array]
 }
 function destructivelyAddElementToBeginningOfArray(array,element) {
-  return array.unshift(element)
+  array.unshift(element)
+  return array
 }
 function addElementToEndOfArray(array, element) {
   return [...array,element]
 }
 function destructivelyAddElementToEndOfArray(array,element) {
-  return array.push(element)
+  array.push(element)
+  return array
 }
 
 function accessElementInArray(array,index) {
   return array(index)
 }
 function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+function removeElementFromEndOfArray(array) {
   return array.shift()
 }
