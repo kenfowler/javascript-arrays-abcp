ee8c7e9034e6ea1b90de897db0aa2ebacd908735
Kenneth Fowler <kenfowler@comcast.net>
2018-01-19 20:53:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,3 +11,5 @@ function addElementToEndOfArray(array, element) {
 function destructivelyAddElementToEndOfArray(array,element) {
   return array.push(element)
 }
+
+function accessElementInArray(array,)
