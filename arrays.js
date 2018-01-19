2b7ff6983c729fe6c8c595cb35a9266638b931c4
Kenneth Fowler <kenfowler@comcast.net>
2018-01-19 20:45:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,5 +3,6 @@ function addElementToBeginningOfArray(array, element) {
   return [element,...array]
 }
 function destructivelyAddElementToBeginningOfArray(array,element) {
-  return array = [element,...array]
+  var foo = [element,...array]
+  return foo
 }
